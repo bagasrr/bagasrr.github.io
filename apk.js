1990ba420190f3431203f42fe8d50032a4013be2
bagasrr <bagasramadhan239@gmail.com>
2022-08-15 20:20:18 +0700

    membuat otomatisasi #project dengan javascript not final

--- apk.js
+++ apk.js
@@ -25,17 +25,60 @@ let pCount = pcLength;
 // buat num sama dengan jumlah project
 pCount = pCount < 10 ? "0" + pCount : pCount;
 num.innerText = pCount;
+console.log(pCount);
 
 // baut listener kalo ditambah project nambah 1 dan sebaliknya
+
 plus.addEventListener("click", () => {
+  const newProjectCard = document.createElement("li");
+  newProjectCard.className = "project-card";
+
+  const newThumb = document.createElement("img");
+  newThumb.src = "images/sample.jpg";
+  newThumb.className = "img-thumb";
+
+  const newProjectInfo = document.createElement("div");
+  newProjectInfo.className = "project-info";
+
+  const newProjectName = document.createElement("h2");
+  newProjectName.className = "project-name";
+  const newLink = document.createElement("a");
+  let newTextProjectName = document.createTextNode("Project Name");
+  newLink.appendChild(newTextProjectName);
+  newProjectName.appendChild(newLink);
+  console.log(newProjectName);
+
+  const newProjectDesc = document.createElement("p");
+  newProjectDesc.className = "project-desc";
+  const newTextProjectDesc = document.createTextNode(
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab dolore nam, veniam, tempora placeat aliquam impedit quam tempore harum minus vero alias nemo debitis nostrum cupiditate dolores provident a excepturi!"
+  );
+  newProjectDesc.appendChild(newTextProjectDesc);
+
+  // memasukkan child ke parent project info
+  newProjectInfo.appendChild(newProjectName);
+  newProjectInfo.appendChild(newProjectDesc);
+
+  // memasukkan parent (project info) ke parent Project Card
+  newProjectCard.appendChild(newThumb);
+  newProjectCard.appendChild(newProjectInfo);
+
+  // memasukkan project card ke project container
+  projectContainer.appendChild(newProjectCard);
+
+  // itemBaru.appendChild(isiItem);
+  // itemContainer.appendChild(itemBaru);
+  console.log(pCount);
   pCount++;
   pCount = pCount < 10 ? "0" + pCount : pCount;
   num.innerText = pCount;
-  // pcLength.append = projectCard;
-  // console.log(pcLength);
 });
+
 minus.addEventListener("click", () => {
   if (pCount > 0) {
+    const projectCard1 = (document.querySelector = ".project-card");
+    projectContainer.removeChild(projectCard1);
+
     pCount--;
     pCount = pCount < 10 ? "0" + pCount : pCount;
     num.innerText = pCount;
